5e8b33ad0741bd03af42844a240ce7459e4d1ba2
Sara Nishad Ali <saranishadali2005@gmail.com>
2026-04-30 16:23:01 +0530

    Create ZeroArgumentConstructor.java

---
+++ ZeroArgumentConstructor.java
@@ -0,0 +1,18 @@
+class Student {
+    String name;
+    int age;
+
+    Student() {
+        name = "Unknown";
+        age = 0;
+    }
+
+    void display() {
+        System.out.println(name + " " + age);
+    }
+
+    public static void main(String[] args) {
+        Student s = new Student();
+        s.display();
+    }
+}
