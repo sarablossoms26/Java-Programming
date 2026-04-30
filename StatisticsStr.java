4b6702b3d7f155d5dd23ab90a542cc9a93e52904
Sara Nishad Ali <saranishadali2005@gmail.com>
2026-04-30 16:47:22 +0530

    Create StatisticsStr.java

---
+++ StatisticsStr.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter string: ");
+        String str = sc.nextLine();
+
+        int upper = 0, lower = 0, digits = 0, spaces = 0;
+
+        for (char ch : str.toCharArray()) {
+            if (Character.isUpperCase(ch))
+                upper++;
+            else if (Character.isLowerCase(ch))
+                lower++;
+            else if (Character.isDigit(ch))
+                digits++;
+            else if (ch == ' ')
+                spaces++;
+        }
+
+        System.out.println("Uppercase: " + upper);
+        System.out.println("Lowercase: " + lower);
+        System.out.println("Digits: " + digits);
+        System.out.println("Spaces: " + spaces);
+    }
+}
