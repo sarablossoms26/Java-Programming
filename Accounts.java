122f1bafce8475386c0b975c2731b42d5ee61d3a
Sara Nishad Ali <saranishadali2005@gmail.com>
2026-04-30 17:03:46 +0530

    Create Accounts.java

---
+++ Accounts.java
@@ -0,0 +1,87 @@
+import java.util.Scanner;
+
+abstract class Accounts {
+    double balance;
+    int accountNumber;
+    String accountHoldersName;
+    String address;
+
+    Accounts(double balance, int accountNumber, String accountHoldersName, String address) {
+        this.balance = balance;
+        this.accountNumber = accountNumber;
+        this.accountHoldersName = accountHoldersName;
+        this.address = address;
+    }
+
+    abstract void withdrawal(double amount);
+    abstract void deposit(double amount);
+
+    void display() {
+        System.out.println("Account Number: " + accountNumber);
+        System.out.println("Balance: " + balance);
+    }
+}
+
+class SavingsAccount extends Accounts {
+    double rateOfInterest;
+
+    SavingsAccount(double balance, int accountNumber, String accountHoldersName, String address, double rateOfInterest) {
+        super(balance, accountNumber, accountHoldersName, address);
+        this.rateOfInterest = rateOfInterest;
+    }
+
+    void withdrawal(double amount) {
+        if (amount <= balance) {
+            balance -= amount;
+            System.out.println("Withdrawn: " + amount);
+        } else {
+            System.out.println("Insufficient Balance");
+        }
+    }
+
+    void deposit(double amount) {
+        balance += amount;
+        System.out.println("Deposited: " + amount);
+    }
+
+    void calculateAmount() {
+        double interest = balance * rateOfInterest / 100;
+        balance += interest;
+        System.out.println("Amount after Interest: " + balance);
+    }
+}
+
+class AccountDemo {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter Account Number: ");
+        int accNo = sc.nextInt();
+        sc.nextLine();
+
+        System.out.print("Enter Account Holder Name: ");
+        String name = sc.nextLine();
+
+        System.out.print("Enter Address: ");
+        String address = sc.nextLine();
+
+        System.out.print("Enter Balance: ");
+        double balance = sc.nextDouble();
+
+        System.out.print("Enter Rate of Interest: ");
+        double rate = sc.nextDouble();
+
+        SavingsAccount s = new SavingsAccount(balance, accNo, name, address, rate);
+
+        System.out.print("Enter Deposit Amount: ");
+        s.deposit(sc.nextDouble());
+
+        System.out.print("Enter Withdrawal Amount: ");
+        s.withdrawal(sc.nextDouble());
+
+        s.calculateAmount();
+        s.display();
+
+        sc.close();
+    }
+}
