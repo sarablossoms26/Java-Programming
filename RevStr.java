b0ecdee96fbf4eac9377472087477f754cb3508f
Sara Nishad Ali <saranishadali2005@gmail.com>
2026-04-30 17:20:03 +0530

    Add files via upload

--- RevStr.java
+++ RevStr.java
@@ -7,7 +7,7 @@ public class Main {
 
         for (int i = words.length - 1; i >= 0; i--) {
             reversed += words[i] + " ";
-        }
+        } 
 
         System.out.println(reversed.trim());
     }
