ad5ac247948bb4f543a869ec741e6936d94c0453
Sara Nishad Ali <saranishadali2005@gmail.com>
2026-04-30 16:46:01 +0530

    Create Circle.java

---
+++ Circle.java
@@ -0,0 +1,32 @@
+class Point {
+    int x, y;
+
+    Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+}
+
+class Circle {
+    Point p;
+    double radius;
+
+    Circle(int x, int y, double r) {
+        p = new Point(x, y);
+        radius = r;
+    }
+
+    void display() {
+        double area = Math.PI * radius * radius;
+        System.out.println("Center: (" + p.x + "," + p.y + ")");
+        System.out.println("Radius: " + radius);
+        System.out.println("Area: " + area);
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Circle c = new Circle(2, 3, 5);
+        c.display();
+    }
+}
