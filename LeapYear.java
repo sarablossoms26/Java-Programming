c1ef99e9d8c1b89eb86642fc491c8152be44f79a
Sara Nishad Ali <saranishadali2005@gmail.com>
2026-04-30 17:25:40 +0530

    Add files via upload

--- LeapYear.java
+++ LeapYear.java
@@ -4,7 +4,7 @@ public class FileFour {
         if((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0))
             System.out.println(year + "is a leap year");
         else
-            System.out.println(year + "is not a leap year");
+            System.out.println(year + "is not a leap year"); 
     }
     public static void main(String[] args) {
         FileFour obj = new FileFour();
