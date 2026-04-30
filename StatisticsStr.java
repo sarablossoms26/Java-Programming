58ae5905644491f64d887abf50e8e4649706af25
Sara Nishad Ali <saranishadali2005@gmail.com>
2026-04-30 17:19:37 +0530

    Add files via upload

--- StatisticsStr.java
+++ StatisticsStr.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter string: ");
-        String str = sc.nextLine();
+        String str = sc.nextLine(); 
 
         int upper = 0, lower = 0, digits = 0, spaces = 0;
 
